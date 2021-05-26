162bc0dc74de9162235384b884ac6163f9db7a7d
AlexDalencourt <alex.dalencourt@gmail.com>
2021-05-26 18:54:33 +0200

    remove unecessary code

--- src/main/java/munchkin/integrator/domain/boards/AssetIndex.java
+++
@@ -1,6 +0,0 @@
-package munchkin.integrator.domain.boards;
-
-public class AssetIndex {
-    private int col;
-    private int line;
-}
--- src/main/java/munchkin/integrator/domain/creatures/UnfortunateIncident.java
+++
@@ -1,9 +0,0 @@
-package munchkin.integrator.domain.creatures;
-
-import munchkin.integrator.domain.Ability;
-
-public class UnfortunateIncident extends Ability {
-    public UnfortunateIncident(String abilityDescription) {
-        super("Incident Fâcheux", abilityDescription);
-    }
-}
